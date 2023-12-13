e3c12c958143e0e0156e35fe1b912bd1e1f4493e
vandaeleangel <angel.vandaele@student.howest.be>
2023-12-13 10:16:55 +0100

    Adjust link url

--- App.js
+++ App.js
@@ -1,41 +1,43 @@
-import { StyleSheet} from "react-native";
-import { NavigationContainer  } from "@react-navigation/native";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { useEffect } from "react";
-import dynamicLinks from '@react-native-firebase/dynamic-links';
+import dynamicLinks from "@react-native-firebase/dynamic-links";
 
-import { firebase } from '@react-native-firebase/app';
+import { firebase } from "@react-native-firebase/app";
 
 import HomeScreen from "./screens/homescreen";
 import LoginScreen from "./screens/loginScreen";
 
-
 export const firebaseConfig = {
   apiKey: "AIzaSyBLUPnzaTR9TMXypNu6xWQXA_PxKs289wY",
   projectId: "testdynamiclinks-fa637",
-  appId:"1:599136051758:android:60e865fedcb8a1acd675bf",
-
+  appId: "1:599136051758:android:60e865fedcb8a1acd675bf",
 };
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 } else {
-  firebase.app(); // 
+  firebase.app(); //
 }
-
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   useEffect(() => {
-    console.log("reached useEffect")
+    console.log("reached useEffect");
     dynamicLinks()
       .getInitialLink()
-      .then(link => {
-        if (link.url === 'https://www.google.com/search?client=firefox-b-d&q=succes') {
-          console.log(link)
-         console.log("link is equal to url")
-        } else console.log('reached', link.url)
+      .then((link) => {
+        if (link.url.includes("google")) {
+          console.log(link);
+          console.log("Link contains 'google'");
+        } else {
+          console.log('Link does not contain "google":', link.url);
+        }
+      })
+      .catch((error) => {
+        console.error("Error handling dynamic link:", error);
       });
   }, []);
   return (
